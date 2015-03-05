13db63f3cd685641ff74fbcbd25a5d8331e90e86
Marvin Wright <wright@imbs.uni-luebeck.de>
2015-03-05 15:36:32 +0100

    add test functions

---
+++ tests/test_maxstat.R
@@ -0,0 +1,30 @@
+
+library(maxchisq)
+
+x <- iris$Sepal.Length
+y <- iris$Species
+
+## Possible splits
+minprop <- 0.1
+maxprop <- 0.9
+n <- length(y)
+k <- nlevels(y)
+x_sorted <- sort(x)
+y_sorted <- y[order(x)]
+all_values <- unique(x_sorted)
+quantiles <- quantile(x, c(minprop, maxprop))
+possible_splits <- all_values[all_values > quantiles[1] & all_values < quantiles[2]]
+
+## Compute test statistics
+teststats <- sapply(possible_splits, function(split) {
+  idx <- x <= split
+  observed <- cbind(table(y[idx]), table(y[!idx]))
+  chisq.test(observed)$statistic
+})
+
+## Get maximum
+possible_splits[which.max(teststats)]
+max(teststats)
+
+## Use new function
+maxstat_chisq(y = y, x = x, pval_method = "betensky")
